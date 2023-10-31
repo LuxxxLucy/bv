c81667883dfcf71b7e242f2f6bd562455f2e360e
LU Jialin <luxxxlucy@gmail.com>
2023-10-31 23:37:56 +0800

    Update va-img.rs

--- src/bin/va-img.rs
+++ src/bin/va-img.rs
@@ -1,22 +1,17 @@
 use std::env;
 
+use std::fs::File;
+use std::io;
+use std::io::{BufReader, Read};
+
 use ndarray::Array3;
 
 use image::RgbImage;
-
-
-use std::io;
-use std::io::{
-    Read, 
-    BufReader
-};
-use std::fs::File;
 
 fn read_bytes_from_file(file_name: &str) -> io::Result<Vec<u8>> {
     let f = File::open(file_name)?;
     let mut reader = BufReader::new(f);
     let mut buf = Vec::<u8>::new();
-
     reader.read_to_end(&mut buf)?;
     Ok(buf)
 }
@@ -33,17 +28,21 @@ fn array_to_image(arr: Array3<u8>) -> RgbImage {
 
 fn save_array_as_img(array: Array3<u8>, file_name: &str) {
     let image = array_to_image(array);
-    image.save(file_name).unwrap();
+    image.save(file_name).expect("write output to designated path failed");
 }
 
 fn main() {
     let mut args = env::args().skip(1);
-    // getting argument
-    let input_file_name = args.next().unwrap().parse::<String>().unwrap();
-    let output_file_name = args.next().unwrap().parse::<String>().unwrap();
 
-    const width: usize = 256;
-    const height: usize = 256;
+    // getting argument
+    let input_file_name = args.next().unwrap().parse::<String>()
+        .expect("expect first argument to be the input file path");
+    let output_file_name = args.next().unwrap().parse::<String>()
+        .expect("expect first argument to be the output file path");
+
+    // A byte is u8, thus a 2-tuple of byte is 256 * 256
+    const WIDTH: usize = 256;
+    const HEIGHT: usize = 256;
 
     let bytes = read_bytes_from_file(&input_file_name);
 
@@ -54,25 +53,27 @@ fn main() {
 
     let bytes = bytes.unwrap();
 
-
     // processing
-    let mut array = [0u16; width * height];
+    let mut array = [0u16; WIDTH * HEIGHT];
     for pair in bytes.windows(2) {
-        let (x,y) = (pair[0] as usize, pair[1] as usize);
-        array[x*width + y] += 1;
+        let (x, y) = (pair[0] as usize, pair[1] as usize);
+        array[x * WIDTH + y] += 1;
     }
-    let array = array.iter().map(|v| (*v as f64).log(2.0)).collect::<Vec<_>>();
+    let array = array.iter().map(|v| (*v as f64)).collect::<Vec<_>>();
 
     // normalize the value
     let max = array.iter().max_by(|a, b| a.total_cmp(b)).unwrap();
-    let array = array.iter().map(|v| (*v as f64) / (*max as f64)).collect::<Vec<f64>>();
+    let array = array
+        .iter()
+        .map(|v| (*v as f64).log(10.0) / (*max as f64).log(10.0))
+        .collect::<Vec<f64>>();
 
     // convert it to 3d.
-    let mut array3d: Array3<u8> = Array3::zeros((width, height, 3)); // width x height RGB
-    for ((x, y, z), v) in array3d.indexed_iter_mut() {
+    let mut array3d: Array3<u8> = Array3::zeros((WIDTH, HEIGHT, 3)); // WIDTH x HEIGHT RGB
+    for ((x, y, _z), v) in array3d.indexed_iter_mut() {
         let x = x as usize;
         let y = y as usize;
-        *v = (array[x*width + y] * 255.0).round() as u8
+        *v = (array[x * WIDTH + y] * 255.0).round() as u8
     }
 
     // save
